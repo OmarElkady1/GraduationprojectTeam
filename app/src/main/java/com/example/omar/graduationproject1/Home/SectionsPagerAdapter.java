e5b6f34d2805ad80af31d332186ed895f3441dd6
Omar <pro.omar.elkady@gmail.com>
2019-02-21 00:08:57 +0200

    hello

---
+++ app/src/main/java/com/example/omar/graduationproject1/Home/SectionsPagerAdapter.java
@@ -0,0 +1,31 @@
+package com.example.omar.graduationproject1.Home;
+
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentPagerAdapter;
+
+import java.util.ArrayList;
+import java.util.List;
+/*
+* this class for store fragment for tab
+* */
+
+public class SectionsPagerAdapter extends FragmentPagerAdapter {
+    private final List<Fragment> fragmentList=new ArrayList<>();
+    public SectionsPagerAdapter(FragmentManager fm) {
+        super(fm);
+    }
+
+    @Override
+    public Fragment getItem(int postion) {
+        return fragmentList.get(postion);
+    }
+
+    @Override
+    public int getCount() {
+        return fragmentList.size();
+    }
+    public void  addFragment(Fragment fragment){
+        fragmentList.add(fragment);
+    }
+}
---
+++ app/src/main/java/com/example/omar/graduationproject1/Profile/ProfileActivity.java
@@ -0,0 +1,69 @@
+package com.example.omar.graduationproject1.Profile;
+
+import android.content.Context;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.util.Log;
+import android.view.Menu;
+import android.view.MenuItem;
+
+import com.example.omar.graduationproject1.R;
+import com.example.omar.graduationproject1.Utils.BottomNavigationViewHelper;
+import com.ittianyu.bottomnavigationviewex.BottomNavigationViewEx;
+
+public class ProfileActivity extends AppCompatActivity{
+    private static final String TAG = "ProfileActivity";
+    private static final int ACTIVITY_NUM = 4;
+
+    private Context mContext = ProfileActivity.this;
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_profile);
+        Log.d(TAG, "onCreate: started.");
+
+        //setupBottomNavigationView();
+        setupToolbar();
+    }
+
+    private void setupToolbar(){
+        Toolbar toolbar = (Toolbar) findViewById(R.id.profileToolBar);
+        setSupportActionBar(toolbar);
+
+        toolbar.setOnMenuItemClickListener(new Toolbar.OnMenuItemClickListener() {
+            @Override
+            public boolean onMenuItemClick(MenuItem item) {
+                Log.d(TAG, "onMenuItemClick: clicked menu item: " + item);
+
+                switch (item.getItemId()){
+                    case R.id.profileMenu:
+                        Log.d(TAG, "onMenuItemClick: Navigating to Profile Preferences.");
+                }
+
+                return false;
+            }
+        });
+    }
+
+    /**
+     * BottomNavigationView setup
+     */
+    private void setupBottomNavigationView(){
+        Log.d(TAG, "setupBottomNavigationView: setting up BottomNavigationView");
+        BottomNavigationViewEx bottomNavigationViewEx = (BottomNavigationViewEx) findViewById(R.id.bottomNavViewBar);
+        BottomNavigationViewHelper.setupBottomNavigationView(bottomNavigationViewEx);
+        BottomNavigationViewHelper.enableNavigation(mContext, bottomNavigationViewEx);
+        Menu menu = bottomNavigationViewEx.getMenu();
+        MenuItem menuItem = menu.getItem(ACTIVITY_NUM);
+        menuItem.setChecked(true);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.profile_menu, menu);
+        return true;
+    }
+}
